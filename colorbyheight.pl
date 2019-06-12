728b510f34510388fe9af3f7bb6056e96513eeb6
ponychen <18709821294@outlook.com>
2019-06-12 20:34:27 +0800

    fix bugs

--- colorbyheight.pl
+++ colorbyheight.pl
@@ -1,5 +1,6 @@
 #!perl
-#this script color atoms by their coordinations, please use this with original type 
+#20190612fix bugs and now will output the result to a new file
+#this script color atoms by their coordinations
 #ponychen
 #20190605
 #email:18709821294@outlook.com
@@ -10,22 +11,25 @@ use MaterialsScript qw(:all);
 use POSIX;
 
 #the only parameter you need chage
-my $direction = "c";      #a or b or c means the crystal direction pertenticulay to your oberseving plane
-my $filename = "CONTCAR"; #xsd file name
+my $direction = "b";      #a or b or c means the crystal direction pertenticulay to your oberseving plane
+my $filename = "ribbon1"; #xsd file name
 
 #do not change following codes unles you know what you are doing 
-my $doc = $Documents{"$filename.xsd"};
+my $olddoc = $Documents{"$filename.xsd"};
+my $doc = Documents->New("new$filename.xsd");
+   $doc->Copyfrom($olddoc);
+
 my $lattice = $doc->SymmetryDefinition;
 
 #sort all atoms by one of its direct coordination
 my $atoms = $doc->UnitCell->Atoms;
-my @sortedAt = sort {$a->FractionalXYZ->Z <=> $b->FractionalXYZ->Z} @$atoms;
 
 #bulid own colormap with11 level
 my %mycolormap = (0=>"145 44 238",1=>"255 255 255",2=>"47 79 79",3=>"105 105 105",4=>"0 0 255",5=>"0 206 209",6=>"0 255 0",7=>"255 255 0", 8=>"205 92 92",9=>"255 0 0",10=>"105 89 205");
 
 #change atom color by ordination
-if ($direction == "c"){
+if ($direction eq "c"){
+my @sortedAt = sort {$a->FractionalXYZ->Z <=> $b->FractionalXYZ->Z} @$atoms;
 my $distol = $sortedAt[-1]->FractionalXYZ->Z - $sortedAt[0]->FractionalXYZ->Z;
 
 foreach my $atom (@sortedAt){
@@ -33,7 +37,8 @@ foreach my $atom (@sortedAt){
      my @a = split(" ",$mycolormap{$disatom});
     $atom->Color=RGB($a[0],$a[1],$a[2]);
 }}    
-elsif ($direction == "b"){
+elsif ($direction eq "b"){
+my @sortedAt = sort {$a->FractionalXYZ->Y <=> $b->FractionalXYZ->Y} @$atoms;
 my $distol = $sortedAt[-1]->FractionalXYZ->Y - $sortedAt[0]->FractionalXYZ->Y;
 
 foreach my $atom (@sortedAt){
@@ -42,6 +47,7 @@ foreach my $atom (@sortedAt){
     $atom->Color=RGB($a[0],$a[1],$a[2]);
 }}  
 else{
+my @sortedAt = sort {$a->FractionalXYZ->X <=> $b->FractionalXYZ->X} @$atoms;
 my $distol = $sortedAt[-1]->FractionalXYZ->X - $sortedAt[0]->FractionalXYZ->X;
 
 foreach my $atom (@sortedAt){

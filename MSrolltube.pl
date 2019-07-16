2e15b74255008ef30236c112ba8b1e6dad30cceb
ponychen <18709821294@outlook.com>
2019-07-16 22:15:08 +0800

    add support for systems not rightly alined to cartesian space of MS itself

--- MSrolltube.pl
+++ MSrolltube.pl
@@ -1,4 +1,5 @@
 #!perl
+#20190716 add surpport for system not rightly alined to Cartesian coordination of MS
 #this script roll a film along X or Y axis 
 #ponychen
 #20190703
@@ -9,8 +10,8 @@ use Getopt::Long;
 use MaterialsScript qw(:all);
 
 #only two parameters you need specify
-my $axis = "X";   #for the sake of simplify, only support roll along X axis or Y axis
-my $inifile = "test";  #you file containg film to roll
+my $axis = "Z";   #for the sake of simplify, only support roll along X axis or Y axis
+my $inifile = "20-1";  #you file containg film to roll
 
 #do not change following codes unless you konw what you are doing
 #read the file and get the atomic coordinations
@@ -23,35 +24,77 @@ my $atoms = $findoc->UnitCell->Atoms;
 #get the constant value PI
 my $pi = 4*atan2(1,1);
 
+#get the crystal axis cartesian coordination
+my @corx;
+my @cory;
+my @corz;
+for(my $i=0;$i<$atoms->Count; ++$i){
+    $corx[$i] = $lattice->LengthA*@$atoms[$i]->FractionalXYZ->X;
+    $cory[$i] = $lattice->LengthB*@$atoms[$i]->FractionalXYZ->Y;    
+    $corz[$i] = $lattice->LengthC*@$atoms[$i]->FractionalXYZ->Z;}
+
+#the tuple for crystal coordination
+my @fracx;
+my @fracy;
+my @fracz;
+
+#get the coordination matrix of three bias axis
+my @vec;
+    $vec[0] = $inidoc->SymmetryDefinition->VectorA->X;
+    $vec[1] = $inidoc->SymmetryDefinition->VectorB->X;
+    $vec[2] = $inidoc->SymmetryDefinition->VectorC->X;
+    $vec[3] = $inidoc->SymmetryDefinition->VectorA->Y;
+    $vec[4] = $inidoc->SymmetryDefinition->VectorB->Y;
+    $vec[5] = $inidoc->SymmetryDefinition->VectorC->Y;
+    $vec[6] = $inidoc->SymmetryDefinition->VectorA->Z;
+    $vec[7] = $inidoc->SymmetryDefinition->VectorB->Z;
+    $vec[8] = $inidoc->SymmetryDefinition->Vectorc->z;
+
+#get the min and max value of Z direction
+my $minZ = $corz[0]; 
+my $maxZ = $corz[0];
+foreach my $cor (@corz){
+    if($cor < $minZ){
+        $minZ = $cor;}
+    if($cor > $maxZ){
+        $maxZ = $cor;}}        
+
 #start to roll
 if($axis eq "Y"){
     my $R = $lattice->LengthA/(2*$pi); #get the radius of rolled tube
-    my $minZ = @$atoms[0]->Z;  #get the min and max value of Z direction
-    my $maxZ = @$atoms[0]->Z;
-    foreach my $atom (@$atoms){
-        if($atom->Z < $minZ){
-            $minZ = $atom->Z;}
-        if($atom->Z > $maxZ){
-            $maxZ = $atom->Z;}}
-
+    my $i = 0;
     foreach my $atom (@$atoms){
         my $theta = 2*$pi*$atom->FractionalXYZ->X;
-        $atom->X = ($R-($atom->Z-$minZ))*cos($theta-$pi/2)+$lattice->LengthA/2;
-        $atom->Z = ($R-($atom->Z-$minZ))*(1+sin($theta-$pi/2))+($atom->Z-$minZ)+$lattice->LengthC/2-$R;}}
+        $fracx[$i] = (($R-($corz[$i]-$minZ))*cos($theta-$pi/2)+$lattice->LengthA/2)/$lattice->LengthA;
+        $fracy[$i] = $atom->FractionalXYZ->Y;
+        $fracz[$i] = (($R-($corz[$i]-$minZ))*(1+sin($theta-$pi/2))+($corz[$i]-$minZ)+$lattice->LengthC/2-$R)/$lattice->LengthC;
+        $i = $i + 1}
+    
+    my $m = 0;   
+    #transfer the fractional XYZ to cartesian, due to the fact that the atoms coordination in xsd file can only be updated by set the cartesian coordination of specific atom 
+    foreach my $atom (@$atoms){
+        $atom->X = $fracx[$m]*$vec[0] + $fracy[$m]*$vec[1] + $fracz[$m]*$vec[2];
+        $atom->Y = $fracx[$m]*$vec[3] + $fracy[$m]*$vec[4] + $fracz[$m]*$vec[5];
+        $atom->Z = $fracx[$m]*$vec[6] + $fracy[$m]*$vec[7] + $fracz[$m]*$vec[8];
+        $m = $m + 1}}
+                            
 elsif($axis eq "X"){ 
     my $R = $lattice->LengthB/(2*$pi);
-    my $minZ = @$atoms[0]->Z;
-    my $maxZ = @$atoms[0]->Z;
-    foreach my $atom (@$atoms){
-        if($atom->Z < $minZ){
-            $minZ = $atom->Z;}
-        if($atom->Z > $maxZ){
-            $maxZ = $atom->Z;}}
 
+    my $i = 0;
     foreach my $atom (@$atoms){
         my $theta = 2*$pi*$atom->FractionalXYZ->Y;
-        $atom->Y = ($R-($atom->Z-$minZ))*cos($theta-$pi/2)+$lattice->LengthB/2;
-        $atom->Z = ($R-($atom->Z-$minZ))*(1+sin($theta-$pi/2))+($atom->Z-$minZ)+$lattice->LengthC/2-$R;}}
+        $fracy[$i] = (($R-($corz[$i]-$minZ))*cos($theta-$pi/2)+$lattice->LengthB/2)/$lattice->LengthB;
+        $fracx[$i] = $atom->FractionalXYZ->X;
+        $fracz[$i] = (($R-($corz[$i]-$minZ))*(1+sin($theta-$pi/2))+($corz[$i]-$minZ)+$lattice->LengthC/2-$R)/$lattice->LengthC;
+        $i = $i + 1}
+    
+    my $m = 0;    
+    foreach my $atom (@$atoms){
+        $atom->X = $fracx[$m]*$vec[0] + $fracy[$m]*$vec[1] + $fracz[$m]*$vec[2];
+        $atom->Y = $fracx[$m]*$vec[3] + $fracy[$m]*$vec[4] + $fracz[$m]*$vec[5];
+        $atom->Z = $fracx[$m]*$vec[6] + $fracy[$m]*$vec[7] + $fracz[$m]*$vec[8];
+        $m = $m + 1}}
+ 
 else{
-    #do nothing
-    last}                               
+    print "please reset $axis, only support X or Y for convernence!!!\n"}
